a954965c6e6ed1a455f5b29b5b3d0c6f305bc059
dtulumidis <dextavision@gmail.com>
2020-12-02 16:15:09 +0100

    solved day 1

---
+++ day_1/solution_day_1.js
@@ -0,0 +1,29 @@
+// Task:
+// Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+var fs = require('fs');
+var inputArray = fs.readFileSync('input.txt').toString().split("\n");
+
+// Convert to Int
+inputArray = inputArray.map(number => parseInt(number));
+
+const MATCH_NUMBER = 2020;
+
+let first_number = 0;
+let second_number = 0;
+
+inputArray.forEach((num) => {
+  inputArray.some((secNumb) => {
+    if (num === secNumb) {
+      return;
+    }
+
+    if (num + secNumb === MATCH_NUMBER) {
+      first_number = num;
+      second_number = secNumb;
+      return true;
+    }
+  });
+});
+
+console.log(first_number * second_number);
